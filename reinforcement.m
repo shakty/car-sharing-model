1fbd3a7bea2340daaeaeb9589e40e3387eee7967
Stefano Balietti <futur.dorko@gmail.com>
2016-03-09 18:34:39 +0100

    repetitions and more variables for learning model

--- reinforcement.m
+++ reinforcement.m
@@ -30,23 +30,7 @@ avail_strategies_time = 0:60;
 % Nr available time strategies.
 nr_strategies_time = length(avail_strategies_time);
 
-% Initial propensities for car/bus for each strategy of each player.
-propensities_carbus = ones(N, nr_strategies);
 
-% Initial propensities for time for each strategy of each player.
-propensities_time = ones(N, nr_strategies_time);
-
-% Probabilities to choose car vs bus. (all equally probable at t=0).
-probabilities = ones(N, nr_strategies)*(1/nr_strategies);
-
-% Probabilities to choose a time. (4all equally probable at t=0).
-probabilities_time = ones(N, nr_strategies_time)*(1/nr_strategies_time);
-
-% Store strategies, payoffs and choices over all rounds.
-payoffs = zeros(N, T);
-choices = zeros(N, T);
-strategies_carbus = ones(N, T);
-strategies_time = ones(N, T);
 
 % Game Variables.
 
@@ -66,15 +50,60 @@ SLOPE_CAR_MISS = PAYOFF_CAR / 60;
 % Propensity increase for choosing bus.
 INCREASE_BUS = 5;
 
-% Propensity increase of neighboring times when choosing a car.
-% (The increment is deducted if the player does not find a car)
+% Propensity increase of neighboring times when getting a car.
 INCREASE_TIME = 10;
+
+% Propensity decrease of neighboring times when missing a car.
+DECREASE_TIME = 10;
+
+% How many neighboring times are updated by INCREASE_TIME.
+TIME_INTERVAL_INCREASE = 10;
+
+% How many neighboring times are updated by DECREASE_TIME.
+TIME_INTERVAL_DECREASE = 10;
 
 % Propensity increase for the BUS, when the player does not find a car.
 INCREASE_CAR_MISSED = 10;
 
 % Propensity incraese for the CAR, when the player finds a car.
 INCREASE_CAR_GOT = 10;
+
+
+% Repetition settings.
+
+% Number of times to re-run the model.
+REPETITIONS = 30;
+
+% Save final outcome each simulation.
+
+rep_nCars = zeros(REPETITIONS, 1);
+rep_avgCarTime = zeros(REPETITIONS, 1);
+rep_depCarTime = zeros(REPETITIONS, N);
+rep_propensities_carbus = zeros(N, 2, REPETITIONS);
+rep_propensities_time = zeros(N, nr_strategies_time, REPETITIONS);
+
+for r = 1 : REPETITIONS
+
+% Clear simulation data.
+    
+% Initial propensities for car/bus for each strategy of each player.
+propensities_carbus = ones(N, nr_strategies);
+
+% Initial propensities for time for each strategy of each player.
+propensities_time = ones(N, nr_strategies_time);
+
+% Probabilities to choose car vs bus. (all equally probable at t=0).
+probabilities = ones(N, nr_strategies)*(1/nr_strategies);
+
+% Probabilities to choose a time. (4all equally probable at t=0).
+probabilities_time = ones(N, nr_strategies_time)*(1/nr_strategies_time);
+
+% Store strategies, payoffs and choices over all rounds.
+payoffs = zeros(N, T);
+choices = zeros(N, T);
+strategies_carbus = ones(N, T);
+strategies_time = ones(N, T);
+    
 
 for t = 1 : T
     
@@ -144,8 +173,11 @@ for t = 1 : T
                 propensities_carbus(idx, CAR) + INCREASE_CAR_GOT;            
                 % TODO: could do an in increase proportional payoff.
                 
+                
                 increase = INCREASE_TIME;
-                for i = (time+1) : min(time+20, nr_strategies_time)
+                limit = min(time + TIME_INTERVAL_INCREASE, ...
+                            nr_strategies_time);
+                for i = (time+1) : limit
                     propensities_time(idx, i) = ...
                         propensities_time(idx, i) + increase;
                     increase = increase - 2;
@@ -162,8 +194,9 @@ for t = 1 : T
                 propensities_carbus(idx, BUS) + INCREASE_CAR_MISSED;            
                 % TODO: could do an in increase proportional to payoff.
             
-                increase = INCREASE_TIME;
-                for i = max(time-20, 1) : (time-1)
+                increase = DECREASE_TIME;                
+                limit = max(time - TIME_INTERVAL_DECREASE, 1);
+                for i = limit : (time-1)
                     propensities_time(idx, i) = ...
                         propensities_time(idx, i) + increase;
                     increase = increase - 2;
@@ -205,13 +238,23 @@ for t = 1 : T
      
 end
 
-% Propensities.
-propensities_carbus
-
 carPlayers = find(strategies_carbus(:,t) == CAR);
+avgDepTimeCar = mean(strategies_time(carPlayers,t));
 
-avgDepTimeCar = mean(strategies_time(carPlayers,t))
+rep_nCars(r) = length(carPlayers);
+rep_avgCarTime(r) = avgDepTimeCar;
+rep_depCarTime(r,:) = strategies_time(:,t);
+rep_propensities_carbus(:,:,r) = propensities_carbus;
+rep_propensities_time(:,:,r) = propensities_time;
 
+
+
+end
+
+% Stats.
+
+avgCar = mean(rep_nCars)
 % How close to equilibrium?
-EQ = CAR_NUMBER - length(carPlayers)
+EQ = CAR_NUMBER - avgCar
 
+avgDepTimeCar = mean(rep_avgCarTime)
